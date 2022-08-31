a7ed39658198f13e48bca2a94cd4bf3dddf79601
yoannchb-pro <charbonn.yoann@gmail.com>
2022-08-31 11:58:05 -0400

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# game-of-life
+# Game-of-life
+
 Game of life algo
+
+> Note: See example [here](https://yoannchb-pro.github.io/game-of-life/index.html)
+
+## Import
+
+```html
+<script src="https://cdn.jsdelivr.net/gh/yoannchb-pro/game-of-life/index.min.js"></script>
+```
+
+## How to use
+
+There are two arguments `nbCelssWidth` and `nbCellsHeight` (by default 50 and 50) for the number of cells you want on the width and on the height
+
+```js
+const handler = new GameOfLife(3, 3);
+console.log(handler.game);
+//[[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+```
+
+### Random generation
+
+There is one argument `factor` (by default 0.2) between 0 and 1 that determine the probability of a cell to be alive
+
+```js
+handler.randomGeneration();
+//[[0, 0, 1], [1, 0, 1], [1, 0, 0]]
+```
+
+### Evoluate
+
+Evoluate the environment
+
+```js
+//[[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+handler.evoluate();
+//[[0, 0, 0], [1, 1, 1], [0, 0, 0]]
+```
+
+### Hydratate
+
+There are two arguments `line` and `column`
+
+```js
+handler.hydratate(1, 2);
+//[[0, 0, 0], [0, 0, 1], [0, 0, 0]]
+```
+
+### Deshydratate
+
+There are two arguments `line` and `column`
+
+```js
+//[[0, 0, 0], [0, 0, 1], [0, 0, 0]]
+handler.deshydratate(1, 2);
+//[[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+```
+
+### Clear
+
+Clear the whole environment
+
+```js
+//[[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+handler.clear();
+//[[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+```
---
+++ index.js
@@ -0,0 +1,90 @@
+class GameOfLife {
+  constructor(nbCellsWidth, nbCellsHeight) {
+    this.nbCellsWidth = nbCellsWidth ?? 50;
+    this.nbCellsHeight = nbCellsHeight ?? 50;
+    this.game = [];
+
+    this.initGame();
+  }
+
+  initGame() {
+    for (let y = 0; y < this.nbCellsWidth; ++y) {
+      this.game.push([]);
+      for (let x = 0; x < this.nbCellsHeight; ++x) {
+        this.game[y].push(0);
+      }
+    }
+  }
+
+  clear() {
+    this.game = [];
+    this.initGame();
+  }
+
+  randomGeneration(factor = 0.2) {
+    for (let y = 0; y < game.nbCellsWidth; ++y) {
+      for (let x = 0; x < game.nbCellsHeight; ++x) {
+        if (Math.random() < factor) this.hydratate(y, x);
+        else this.deshydratate(y, x);
+      }
+    }
+  }
+
+  deshydratate(line, column) {
+    this.game[line][column] = 0;
+  }
+
+  hydratate(line, column) {
+    this.game[line][column] = 1;
+  }
+
+  getCase(line, column) {
+    return this.game[line]?.[column] ?? 0;
+  }
+
+  getNeighboursLength(line, column) {
+    let neighbours = 0;
+
+    //left top
+    if (this.getCase(line - 1, column - 1)) ++neighbours;
+    //top
+    if (this.getCase(line - 1, column)) ++neighbours;
+    //right top
+    if (this.getCase(line - 1, column + 1)) ++neighbours;
+
+    //left
+    if (this.getCase(line, column - 1)) ++neighbours;
+    //right
+    if (this.getCase(line, column + 1)) ++neighbours;
+
+    //bottom left
+    if (this.getCase(line + 1, column - 1)) ++neighbours;
+    //bottom
+    if (this.getCase(line + 1, column)) ++neighbours;
+    //bottom right
+    if (this.getCase(line + 1, column + 1)) ++neighbours;
+
+    return neighbours;
+  }
+
+  evoluate() {
+    const copy = JSON.parse(JSON.stringify(this.game));
+
+    for (let y = 0; y < this.game.length; ++y) {
+      const line = this.game[y];
+      for (let x = 0; x < line.length; ++x) {
+        const column = line[x];
+
+        const neighboursLength = this.getNeighboursLength(y, x);
+
+        if (column) {
+          if (neighboursLength !== 2 && neighboursLength !== 3) copy[y][x] = 0;
+        } else {
+          if (neighboursLength === 3) copy[y][x] = 1;
+        }
+      }
+    }
+
+    this.game = copy;
+  }
+}
